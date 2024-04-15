3dae00d0fb803e48cd37a6e8ba51af065bf455ba
Johan <johantoerme@live.se>
2024-04-15 20:43:30 +0200

    :sparkles: importing hideCookieBar-function in main.js

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,7 @@ import '../scss/style.scss';
 import toggleNavMenu from './components/navMenu';
 import reviewCarousel from './components/reviewCarousel';
 import { toggleFormBtnStatus, resetFormValues } from './components/input';
+import hideCookieBar from './components/cookiebar';
 
 toggleNavMenu();
 toggleFormBtnStatus();
