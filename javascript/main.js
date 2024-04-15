0f678c684e13d59a8ba156c9f38ec3f27b2c29c3
Johan <johantoerme@live.se>
2024-04-15 20:18:31 +0200

    :sparkles: added cookiebar.js to remove cookie bar when clicking buttons

--- javascript/main.js
+++ javascript/main.js
@@ -2,10 +2,12 @@ import '../scss/style.scss';
 import toggleNavMenu from './components/navMenu';
 import reviewCarousel from './components/reviewCarousel';
 import { toggleFormBtnStatus, resetFormValues }  from './components/input';
+import hideCookieBar from './components/cookiebar';
 
 toggleNavMenu();
 toggleFormBtnStatus();
 resetFormValues();
 reviewCarousel();
+hideCookieBar();
 
 console.log('bajs, for science');
